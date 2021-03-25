52292926f8c930d77315c9f10aea464a594fcdae
hrid0ybd <hridoyahmed11197@gmail.com>
2021-03-25 21:17:04 +0600

    For Loop added

---
+++ ForLoop/ForLoop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i=1; i<number; i++)
+            {
+                if(i%2 == 0)
+                {
+                    Console.WriteLine($"The Number {i} is Even");
+                }
+                else
+                {
+                    Console.WriteLine($"The number {i} is Odd");
+                }
+            }
+        }
+    }
+}
