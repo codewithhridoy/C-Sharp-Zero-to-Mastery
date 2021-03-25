350e80b60f3780ceee06ed34b4b12ab53e1b2eef
hrid0ybd <hridoyahmed11197@gmail.com>
2021-03-25 21:01:50 +0600

    Conditional Logic Added

---
+++ ConditionalLogic/ConditionalLogic/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConditionalLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            if (number == 0)
+            {
+                Console.WriteLine("Your number is Zero");
+            }
+            else if (number > 0)
+            {
+                Console.WriteLine("Your number is Positive");
+            }
+            else
+            {
+                Console.WriteLine("Your number is negative");
+            }
+
+
+        }
+    }
+}
