ef082d6dadc3a00b3429dc2b38a41ea714a8dbdb
hrid0ybd <hridoyahmed11197@gmail.com>
2021-03-25 21:28:38 +0600

    Switch Case Added

---
+++ SwitchCase/SwitchCase/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter you name: ");
+            string input = Console.ReadLine();
+            
+            switch(input)
+            {
+                case "Hridoy":
+                    Console.WriteLine("Admin");
+                    break;
+
+                case "Tanveer":
+                    Console.WriteLine("Moderator");
+                    break;
+
+                default:
+                    Console.WriteLine("User");
+                    break;
+            }
+        }
+    }
+}
