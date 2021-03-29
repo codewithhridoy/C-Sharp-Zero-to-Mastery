89e87799146754d951986218e003833665558025
hrid0ybd <hridoyahmed11197@gmail.com>
2021-03-29 11:05:01 +0600

    While and Do While loop added

---
+++ WhileDoWhileLoop/WhileDoWhileLoop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WhileDoWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            int i=0;
+            Console.WriteLine("While Loop Result: ");
+            while (i < number)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            int j = 1;
+            Console.WriteLine("Do While Loop Result: ");
+            do
+            {
+                Console.WriteLine(j);
+                j++;
+            } while (j < 5);
+        }
+    }
+}
