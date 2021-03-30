b5792aae99e18f8118d9a937059f1e5e0abdb16c
hrid0ybd <hridoyahmed11197@gmail.com>
2021-03-30 19:43:12 +0600

    Foreach Loop Added Again

---
+++ ForeachLoop/ForeachLoop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ForeachLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentName = { "Tasmia", "Mariz", "Pranto", "Mahdin", "Taseen", "Fiba", "Dola" };
+
+            foreach(string name in studentName)
+            {
+                Console.WriteLine(name);
+            }
+      }
+    }
+}
