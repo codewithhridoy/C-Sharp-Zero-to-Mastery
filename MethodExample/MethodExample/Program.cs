38a4d6e2eb445b287a378d5b1fca4585f0a0d11d
hrid0ybd <hridoyahmed11197@gmail.com>
2021-03-30 19:44:08 +0600

    Method example Added

---
+++ MethodExample/MethodExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MethodExample
+{
+    class Program
+    {
+        
+        // Without Parameter
+        static void CalculateNumber() 
+        {
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = num1 + num2;
+            Console.WriteLine($"Sum of {num1} + {num2} = {result} ");
+        }
+
+        // With Parameters
+        static void EvenNumber(int num)
+        {
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Your given number is even.");
+            }
+        }
+
+        // Default Parameter dite chaile method er vitor
+        // parameter er value assign kore dite hobe
+
+        // Return value
+        static int SubTract(int number1 = 10, int number2 = 5)
+        {
+            return (number1 - number2);
+        }
+        static void Main(string[] args)
+        {
+            CalculateNumber();
+            EvenNumber(10);
+            Console.WriteLine(SubTract(15, 7));
+        }
+    }
+}
